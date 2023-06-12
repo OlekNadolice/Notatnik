ad537169758a90b4d4acc779688355a7597aaeef
SaQie <Kamcio99@interia.com>
2023-06-12 09:58:30 +0200

    add user service and repository

---
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -0,0 +1,31 @@
+package com.example.demo.repository;
+
+import com.example.demo.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
+    boolean existsByEmail(String email);
+
+    boolean existsByUsername(String username);
+
+    @Transactional
+    @Modifying
+    @Query("delete from User u where u.id = :id")
+    void deleteById(Long id);
+
+    User findByEmail(String email);
+
+    User findByResetPasswordToken(String token);
+
+    User findByActivationToken(String token);
+}
