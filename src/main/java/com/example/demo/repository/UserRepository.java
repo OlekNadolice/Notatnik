7c3d02cbf1f576d8346a19f4bf449789eb11694c
adammroczkowski <mroczkowskiadammm@gmil.com>
2023-06-13 20:11:21 +0200

    controller, serivice, repository

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -1,11 +1,11 @@
-package com.example.demo.repository;
+package pl.saqie.wNotesApp.repository;
 
-import com.example.demo.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+import pl.saqie.wNotesApp.domain.User;
 
 import java.util.Optional;
 
