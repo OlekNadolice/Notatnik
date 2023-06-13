c12d0619acc26ffa17f4f02270c37dd56cb22192
Kamil75902 <kamil256@protonmail.com>
2023-06-13 21:05:05 +0200

    Add dto

---
+++ src/main/java/com/example/demo/dto/wnotes/NoteDeleteDTO.java
@@ -0,0 +1,18 @@
+package com.example.demo.dto.wnotes;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class NoteDeleteDTO {
+
+    private Long id;
+    private String title;
+    private String content;
+
+}
